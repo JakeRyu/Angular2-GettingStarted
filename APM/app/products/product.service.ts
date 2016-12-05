0c7b57be8adb8d655178e763ba924d0051a76885
Jake <jake.ryu@hotmail.co.uk>
2016-12-05 23:34:11 +0000

    add product-detail routing

--- APM/app/products/product.service.ts
+++ APM/app/products/product.service.ts
@@ -3,11 +3,12 @@ import { IProduct } from './product';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class ProductService{
-    private _productUrl: string = "api/products/products.json";
+    private _productUrl = "api/products/products.json";
 
     constructor(private _http: Http){
     }
@@ -15,7 +16,7 @@ export class ProductService{
     getProducts(): Observable<IProduct[]> {
         return this._http.get(this._productUrl)
                     .map((response: Response) => <IProduct[]>response.json())
-                    //.do((data => console.log("All: " + JSON.stringify(data))))
+                    .do((data: any) => console.log("All: " + JSON.stringify(data)))
                     .catch(this.handleError);
     }
 
